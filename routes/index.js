086dc44587205177efc3153ec1d7854e592b8339
SaiHy2016 <huyuan2016@yeah.net>
2017-06-13 15:34:30 +0800

    mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost/my_database');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '啦啦啦啦' });
+  res.render('index', { title: '我的网站' });
 });
 
 module.exports = router;

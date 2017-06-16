5e64d257e1e06cf985907d7ae3ab3a7690da781a
SaiHy2016 <huyuan2016@yeah.net>
2017-06-16 17:29:48 +0800

    日历签到

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,13 @@ var PersonSchema = new mongoose.Schema({
   title: String,
   content: String
 })
+var checkinSchema = new mongoose.Schema({
+  name:String,
+  time: String,
+})
+
 var PersonModel = db.model('Person',PersonSchema)
+var checkinModel = db.model('checkin',checkinSchema)
 
 
 /* GET home page. */
@@ -17,11 +23,52 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '我的网站' });
 });
 
+router.get('/checkin', function (req, res, next) {
+  let t = new Date(), y = t.getFullYear(), m = t.getMonth() + 1,reg
+  //判断 天 年
+  if (m == 1) {
+    y -= 1
+    reg=`^(${y}-12)|(${y+1}-1|2)-\\d{1,2}\\b`
+  } else if(m==12) {
+    y += 1
+    reg=`^(${y-1}-(11|12))|(${y}-1)-\\d{1,2}\\b`
+  } else {
+    reg=`^${y}-(${m}|${m-1}|${m+1})-\\d{1,2}\\b`
+  }
+  console.log(reg)
+  checkinModel.find({ name: '胡渊', time: { $regex: reg}},function (err, docs) {
+    var log=docs.map(function (v,i) {
+      v.time=v.time.split(/\s/g)[0]
+      return v
+    })
+
+    var days=[31,28,31,30,31,30,31,31,30,31,30,31]
+    if ((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0)){
+      days[1]=29
+    }
+
+    console.log(log)
+    console.log('几号date '+t.getDate())
+    console.log('星期几day '+t.getDay())
+    console.log('这个月几天days'+days)
+    
+    res.render('checkin', {
+      year: t.getFullYear(),
+      month: t.getMonth(),//月份0-11
+      date: t.getDate(),
+      day: t.getDay(),//星期0-6
+      days:days,
+      log: log
+    });
+  })
+});
+
 router.get('/mongodb', function (req, res, next) {
-  PersonModel.find(function(err,docs){
+  PersonModel.find({name:'胡渊'},function(err,docs){
     res.render('mongodb', res.render('mongodb',{lists:docs}));
   })
 });
+
 
 
 //ajax
@@ -51,5 +98,13 @@ router.get('/mongodbsan', function(req, res, next) {
     })
   })
 })
+
+router.get('/check_in', function (req, res, next) { 
+  checkinModel.create({name:'胡渊',time:(new Date()).toLocaleString()}, function (err,docs) {
+    var d=1
+    res.send(`你已签到${d}天`)
+  })
+})
+
 
 module.exports = router;
